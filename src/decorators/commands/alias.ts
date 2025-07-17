9890da454d7bac945cc1a50ff42d91fae14425cd
chrisitopherus <jochchristopher@gmail.com>
2025-07-17 03:33:24 +0200

    implemented first draft of alias decorator for command classes

---
+++ src/decorators/commands/alias.ts
@@ -0,0 +1,27 @@
+import { CLIError } from "../../core/cliError";
+import { CommandClass } from "../../types/core/command";
+import { CommandMetadata } from "../../types/decorators/command";
+import { Metadata, MetadataKey } from "../../utility/metadata";
+
+export function Alias(aliases: string | string[]) {
+    return function (constructor: CommandClass) {
+        const value = typeof aliases === "string" ? [aliases] : aliases;
+        value.forEach(alias => {
+            const cleaned = alias?.trim();
+            if (!cleaned) throw CLIError.factory.invalidCommandAlias("<empty>", constructor.name);
+        });
+
+        Metadata.defineOrUpdateMetadata<CommandMetadata>(
+            MetadataKey.Command,
+            constructor,
+            { aliases: value },
+            (metadata) => {
+                metadata.aliases = metadata.aliases !== undefined
+                    ? [...metadata.aliases, ...value]
+                    : value;
+
+                return metadata;
+            }
+        );
+    }
+}
