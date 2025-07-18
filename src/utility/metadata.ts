17715b84c95a13f098117dd6799f4f25a696ff26
chrisitopherus <jochchristopher@gmail.com>
2025-07-18 12:49:54 +0200

    updated metadata helper to use type map

--- src/utility/metadata.ts
+++ src/utility/metadata.ts
@@ -1,26 +1,27 @@
 import "reflect-metadata";
 import { PropertyName } from "utility-pickle";
+import { MetadataTypeMap } from "../types/utility";
 
 export enum MetadataKey {
     Command,
-    Arg
+    Option
 }
 
 export class Metadata {
-    public static defineOrUpdateMetadata<T>(
-        key: MetadataKey,
+    public static defineOrUpdateMetadata<K extends keyof MetadataTypeMap>(
+        key: K,
         target: Object,
-        defaultValue: T,
-        updateHandler: (metadata: T) => T,
+        defaultValue: MetadataTypeMap[K],
+        updateHandler: (metadata: MetadataTypeMap[K]) => MetadataTypeMap[K],
         propertyKey?: PropertyName
-    ): T {
-        let metadata = this.getMetadata<T>(key, target, propertyKey);
+    ): MetadataTypeMap[K] {
+        let metadata = this.getMetadata(key, target, propertyKey);
         metadata = metadata === undefined ? defaultValue : updateHandler(metadata);
         return this.defineMetadata(key, metadata, target, propertyKey);
     }
 
-    public static getOrInitMetadata<T extends Record<string, any>>(key: MetadataKey, value: T, target: Object, propertyKey?: PropertyName): T {
-        let metadata = this.getMetadata<T>(key, target, propertyKey);
+    public static getOrInitMetadata<K extends keyof MetadataTypeMap>(key: K, value: MetadataTypeMap[K], target: Object, propertyKey?: PropertyName): MetadataTypeMap[K] {
+        let metadata = this.getMetadata(key, target, propertyKey);
 
         if (metadata !== undefined) return metadata;
 
@@ -28,30 +29,30 @@ export class Metadata {
         return value;
     }
 
-    public static combineOrInitMetadata<T extends Record<string, any>>(key: MetadataKey, value: T, target: Object, propertyKey?: PropertyName) {
-        let metadata: T | undefined = propertyKey ? Reflect.getMetadata(key, target, propertyKey) : Reflect.getMetadata(key, target);
+    public static combineOrInitMetadata<K extends keyof MetadataTypeMap>(key: K, value: MetadataTypeMap[K], target: Object, propertyKey?: PropertyName) {
+        let metadata: MetadataTypeMap[K] | undefined = propertyKey ? Reflect.getMetadata(key, target, propertyKey) : Reflect.getMetadata(key, target);
         const combined = metadata ? Object.assign({}, metadata, value) : { ...value };
 
         this.defineMetadata(key, combined, target, propertyKey);
     }
 
-    private static getMetadata<T>(key: MetadataKey, target: Object, propertyKey?: PropertyName): T | undefined {
+    private static getMetadata<K extends keyof MetadataTypeMap>(key: K, target: Object, propertyKey?: PropertyName): MetadataTypeMap[K] | undefined {
         return propertyKey
             ? Reflect.getMetadata(key, target, propertyKey)
             : Reflect.getMetadata(key, target);
     }
 
-    private static defineMetadata<T>(key: MetadataKey, value: T, target: Object, propertyKey?: PropertyName): T {
+    private static defineMetadata<K extends keyof MetadataTypeMap>(key: K, value: MetadataTypeMap[K], target: Object, propertyKey?: PropertyName): MetadataTypeMap[K] {
         if (propertyKey) return this.definePropertyMetadata(key, value, target, propertyKey);
         return this.defineClassMetadata(key, value, target);
     }
 
-    private static defineClassMetadata<T>(key: MetadataKey, value: T, target: Object): T {
+    private static defineClassMetadata<K extends keyof MetadataTypeMap>(key: K, value: MetadataTypeMap[K], target: Object): MetadataTypeMap[K] {
         Reflect.defineMetadata(key, value, target);
         return value;
     }
 
-    private static definePropertyMetadata<T>(key: MetadataKey, value: T, target: Object, propertyKey: PropertyName): T {
+    private static definePropertyMetadata<K extends keyof MetadataTypeMap>(key: K, value: MetadataTypeMap[K], target: Object, propertyKey: PropertyName): MetadataTypeMap[K] {
         Reflect.defineMetadata(key, value, target, propertyKey);
         return value;
     }
