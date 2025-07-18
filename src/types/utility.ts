403af325a10157967b4306a24df6598f1c3ad46c
chrisitopherus <jochchristopher@gmail.com>
2025-07-18 12:49:41 +0200

    added metadataTypeMap

--- src/types/utility.ts
+++ src/types/utility.ts
@@ -1,6 +1,14 @@
 import { PropertyName } from "utility-pickle";
+import { MetadataKey } from '../utility/metadata';
+import { CommandMetadata } from "./decorators/command";
+import { OptionMetadata } from "./decorators/options";
 export type TypeConstructor<T> = (value: string) => T;
 export type TypedPropertyDecorator<T> = <K extends PropertyName, O extends Record<K, T>>(
     target: O,
     propertyKey: K
 ) => void;
+
+export type MetadataTypeMap = {
+    [MetadataKey.Command]: CommandMetadata;
+    [MetadataKey.Option]: OptionMetadata;
+}
