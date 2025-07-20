46425dc269bc4609a8f6934f60f59768afd6eb6b
chrisitopherus <jochchristopher@gmail.com>
2025-07-20 22:52:05 +0200

    added type intersection with object for property decorator

--- src/types/utility.ts
+++ src/types/utility.ts
@@ -6,7 +6,7 @@ export type PropertyKey = string | symbol;
 export type Maybe<T> = T | undefined;
 export type PredicateFunc<T> = (value: T) => boolean;
 export type TypeConstructor<T> = (value: string) => T;
-export type TypedPropertyDecorator<T> = <K extends PropertyKey, O extends Record<K, T>>(
+export type TypedPropertyDecorator<T> = <K extends PropertyKey, O extends Record<K, T> & object>(
     target: O,
     propertyKey: K
 ) => void;
