5ba6e66ad4b32b5b36532439b43165d4cc7e24f1
chrisitopherus <jochchristopher@gmail.com>
2025-07-20 22:52:14 +0200

    implemented option decorator

--- src/decorators/options/option.ts
+++ src/decorators/options/option.ts
@@ -1,6 +1,23 @@
-import { OptionInformation } from "../../types/decorators/options";
+import { CLIError } from "../../core/cliError";
+import { OptionInformation, OptionMetadata } from "../../types/decorators/options";
 import { TypedPropertyDecorator } from "../../types/utility";
+import { merge } from "../../utility/functions/merge";
+import { Metadata, MetadataKey } from "../../utility/metadata";
+import { OptionKind } from "../../utility/options/kind";
 
-export function Option<T>(info: OptionInformation<T>): TypedPropertyDecorator<T> {
-    return function (target, propertyKey) { };
+export function Option<T>(information: OptionInformation<T>): TypedPropertyDecorator<T> {
+    return function (target, propertyKey) {
+        if (information.name === undefined || information.name === "") throw CLIError.factory.missingRequiredInformation(`${target.constructor.name}.${String(propertyKey)}`, "name");
+        if (information.description === undefined || information.description === "") throw CLIError.factory.missingRequiredInformation(`${target.constructor.name}.${String(propertyKey)}`, "description");
+        if (information.type === undefined) throw CLIError.factory.missingRequiredInformation(`${target.constructor.name}.${String(propertyKey)}`, "type");
+
+        const data: OptionMetadata = { ...information, kind: OptionKind.OPTION }
+        Metadata.defineOrUpdateMetadata(
+            MetadataKey.Option,
+            target,
+            data,
+            (metadata) => merge(metadata, data),
+            propertyKey
+        );
+    };
 }
