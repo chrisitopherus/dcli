40400f85e9e1a39f8673e8b6353103bdd24d7f7e
chrisitopherus <jochchristopher@gmail.com>
2025-07-20 02:28:03 +0200

    added source to CLIError

--- src/core/cliError.ts
+++ src/core/cliError.ts
@@ -1,3 +1,4 @@
+import { CLIErrorSource } from "../types/core/error";
 import { CLIErrorFactory } from "../utility/cliErrorFactory";
 import { ErrorCode } from "../utility/errorCode";
 
@@ -5,17 +6,20 @@ export class CLIError extends Error {
     public static readonly factory = CLIErrorFactory;
     public readonly code: ErrorCode;
     public readonly rawMessage: string;
-    public constructor(message: string, code: ErrorCode) {
-        super(`[${code}] ${message}`);
+    public readonly source: CLIErrorSource;
+
+    public constructor(message: string, code: ErrorCode, source: CLIErrorSource) {
+        super(`[${code} in ${source}] ${message}`);
 
         this.code = code;
         this.name = "CLIError";
         this.rawMessage = message;
+        this.source = source;
 
         Object.setPrototypeOf(this, CLIError.prototype);
     }
 
     public override toString(): string {
-        return `[${this.code}] ${this.rawMessage}`;
+        return this.message;
     }
 }
