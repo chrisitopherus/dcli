21ce13690ad2a2e7ac22a55806ea029fa2f36fd6
chrisitopherus <jochchristopher@gmail.com>
2025-07-23 00:53:39 +0200

    implemented cli loader

--- src/core/cliLoader.ts
+++ src/core/cliLoader.ts
@@ -1,3 +1,67 @@
-export class CLILoader {
+import "reflect-metadata";
+import { CommandClass } from "../types/core/command";
+import { Metadata, MetadataKey } from "../utility/metadata";
+import { CLIError } from './cliError';
+import { CLICommand } from "./commands/command";
+import { PropertyKey } from "../types/utility";
+import { LoadedCommand, LoadedOption } from "../types/core/loader";
+import { ensure } from "../utility/functions/ensure";
 
+export class CLILoader {
+    private constructor() { }
+
+    public static load(commands: CommandClass[]): LoadedCommand[] {
+        return commands.map((command) => {
+            const metadata = this.getCommandMetadata(command);
+            if (metadata === undefined) throw CLIError.factory.invalidCommand(command.name);
+
+            // load possible options of command
+            const cmdInstance = new command();
+            const loadedOptions = this.loadOptions(cmdInstance);
+            const subcommands = metadata.subcommands ? this.load(metadata.subcommands) : [];
+
+            return {
+                name: ensure(metadata.name, () => CLIError.factory.missingRequiredInformation(command.name, "name", "loader")),
+                description: ensure(metadata.description, () => CLIError.factory.missingRequiredInformation(command.name, "description", "loader")),
+                commandInstance: cmdInstance,
+                subcommands,
+                default: metadata.default ?? false,
+                hidden: metadata.hidden ?? false,
+                aliases: metadata.aliases ?? [],
+                options: loadedOptions
+            }
+        });
+    }
+
+    private static loadOptions(command: CLICommand): LoadedOption[] {
+        const props = Object.getOwnPropertyNames(command);
+        const loadedOptions: LoadedOption[] = [];
+        props.forEach((property) => {
+            const metadata = this.getOptionMetadata(command, property);
+            if (!metadata) return;
+
+            loadedOptions.push(
+                {
+                    name: ensure(metadata.name, () => CLIError.factory.missingRequiredInformation(`${command.constructor.name}.${property}`, "name", "loader")),
+                    description: ensure(metadata.description, () => CLIError.factory.missingRequiredInformation(`${command.constructor.name}.${property}`, "description", "loader")),
+                    type: ensure(metadata.type, () => CLIError.factory.missingRequiredInformation(`${command.constructor.name}.${property}`, "type", "loader")),
+                    required: metadata.required ?? false,
+                    aliases: metadata.aliases ?? [],
+                    propertyName: property,
+                    kind: metadata.kind,
+                    allowedValues: metadata.allowedValues ?? []
+                }
+            );
+        });
+
+        return loadedOptions;
+    }
+
+    private static getCommandMetadata(command: CommandClass) {
+        return Metadata.getMetadata(MetadataKey.Command, command);
+    }
+
+    private static getOptionMetadata(command: CLICommand, propertyKey: PropertyKey) {
+        return Metadata.getMetadata(MetadataKey.Option, command, propertyKey);
+    }
 }
