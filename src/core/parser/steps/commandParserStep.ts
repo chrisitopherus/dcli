e4ca475cd5d905865ed32ef2617205b573e86fdd
chrisitopherus <jochchristopher@gmail.com>
2025-07-25 15:20:33 +0200

    implemented command parser step

--- src/core/parser/steps/commandParserStep.ts
+++ src/core/parser/steps/commandParserStep.ts
@@ -1,14 +1,43 @@
-import { ParserStep } from "../../../types/core/parser";
+import { LoadedCommand } from "../../../types/core/loader";
+import { ParserStep, ParserStepResult } from "../../../types/core/parser";
 import { Maybe } from "../../../types/utility";
-import { ParserContext } from "../parserContext";
+import { ErrorCode } from "../../../utility/errorCode";
+import { CLIError } from "../../cliError";
+import { CommandRegistry } from "../../registry/commandRegistry";
+import { ParserContext } from '../parserContext';
+import { DetermineOptionParserStep } from "./determineOptionParserStep";
 
 export class CommandParserStep implements ParserStep {
-    public constructor() { }
+    private readonly registry: CommandRegistry;
+    public constructor(commandRegistry: CommandRegistry) {
+        this.registry = commandRegistry;
+    }
 
-    handle(context: ParserContext): Maybe<ParserStep> {
-        const token = context.currentToken;
-        if (!token) return;
-        
+    public handle(context: ParserContext): ParserStepResult {
+        const command = this.getCommand(context);
+        if (!command) return {
+            success: false,
+            error: new CLIError("", ErrorCode.PARSER_UNKNOWN_FLAG, "parser")
+        }
+
+        context.progress.commandInformation = command;
+
+        context.progress.command = {
+            commandInstance: command.commandInstance,
+            options: []
+        }
+
+        return {
+            success: true,
+            next: new DetermineOptionParserStep()
+        }
+    }
+
+    private getCommand(context: ParserContext): Maybe<LoadedCommand> {
+        const tokens = context.getAllRemainingToken();
+        const result = this.registry.findCommand(tokens);
+        context.forward(result.matchedCount);
+        return result.command;
     }
 
 }
