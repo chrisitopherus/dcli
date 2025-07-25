33838b796b01977a4c9b3f8cffc4a7d917a00158
chrisitopherus <jochchristopher@gmail.com>
2025-07-25 15:22:24 +0200

    implemented first version of OptionParserStep

---
+++ src/core/parser/steps/optionParserStep.ts
@@ -0,0 +1,36 @@
+import { LoadedOption } from "../../../types/core/loader";
+import { ParserStep, ParserStepResult } from "../../../types/core/parser";
+import { OptionKind } from "../../../utility/options/kind";
+import { CLIError } from "../../cliError";
+import { ParserContext } from "../parserContext";
+import { DetermineOptionParserStep } from "./determineOptionParserStep";
+
+export class OptionParserStep implements ParserStep {
+    private readonly optionInformation: LoadedOption;
+    public constructor(optionInformation: LoadedOption) {
+        this.optionInformation = optionInformation;
+    }
+
+    public handle(context: ParserContext): ParserStepResult {
+        const token = context.consume();
+        if (!token) return {
+            success: false,
+            // TODO: create right error
+            error: CLIError.factory.invalidCommand("No value.")
+        }
+
+        const value = this.optionInformation.type(token) as string;
+        context.progress.command?.options.push({
+            name: this.optionInformation.name,
+            kind: OptionKind.OPTION,
+            value,
+            raw: token
+        });
+
+        return {
+            success: true,
+            next: new DetermineOptionParserStep()
+        };
+    }
+
+}
