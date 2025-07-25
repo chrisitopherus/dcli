73bca5dce64a55eb072aeda269ea1fd50c5e4cb1
chrisitopherus <jochchristopher@gmail.com>
2025-07-25 15:20:13 +0200

    refactored parser context

--- src/core/parser/parserContext.ts
+++ src/core/parser/parserContext.ts
@@ -1,31 +1,51 @@
-import { ParsedCommand } from "../../types/core/parser";
+import { ParserProgress } from "../../types/core/parser";
+import { Maybe } from "../../types/utility";
 
 export class ParserContext {
     private index = 0;
     private args: string[];
 
-    public command: ParsedCommand = {
-        name: "",
-        options: []
-    }
+    public progress: ParserProgress = {
+        positionalIndex: 0
+    };
 
     public constructor(args: string[]) {
         this.args = args;
     }
 
-    public get currentIndex(): number{
+    public get currentIndex(): number {
         return this.index;
     }
 
     public next() {
-        return this.index++;
+        return ++this.index;
     }
 
-    public currentToken(): string | undefined {
+    public back() {
+        return --this.index;
+    }
+
+    public move(index: number) {
+        this.index = index;
+    }
+
+    public forward(amount: number) {
+        this.index += amount;
+    }
+
+    public peek(): Maybe<string> {
         return this.args[this.index];
+    }
+
+    public consume(): Maybe<string> {
+        return this.args[this.index++];
     }
 
     public hasMore(): boolean {
         return this.index < this.args.length;
+    }
+
+    public getAllRemainingToken() {
+        return this.args.slice(this.index);
     }
 }
