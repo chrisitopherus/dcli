a8529a7c89531674cdf503d0fb7b0c79cef07b86
chrisitopherus <jochchristopher@gmail.com>
2025-07-26 18:53:05 +0200

    added parser state

---
+++ src/core/parser/parserState.ts
@@ -0,0 +1,35 @@
+import { LoadedCommand, LoadedOption } from "../../types/core/loader";
+import { ParsedOption } from "../../types/core/parser";
+import { Maybe } from "../../types/utility";
+import { CLICommand } from "../commands/command";
+
+export class ParserState {
+
+    private _availableOptions?: Maybe<LoadedOption[]>;
+    public commandInstance?: Maybe<CLICommand>;
+    public options: ParsedOption[] = [];
+    public positionalIndex = 0;
+    public constructor() { }
+
+    public reset() {
+        this._availableOptions = undefined;
+        this.commandInstance = undefined;
+        this.options = [];
+        this.positionalIndex = 0;
+    }
+
+    public resetForNewCommand(command: LoadedCommand) {
+        this._availableOptions = command.options;
+        this.commandInstance = command.commandInstance;
+        this.options = [];
+        this.positionalIndex = 0;
+    }
+
+    public addOption(option: ParsedOption) {
+        this.options.push(option);
+    }
+
+    public get availableOptions() {
+        return this._availableOptions ?? [];
+    }
+}
