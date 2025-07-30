d08c7e3be63c5044c5d03f1a10aa876c0286fbd4
chrisitopherus <jochchristopher@gmail.com>
2025-07-30 19:13:03 +0200

    removed incorrect condition, should not throw if position is set

--- src/decorators/options/positional.ts
+++ src/decorators/options/positional.ts
@@ -9,7 +9,7 @@ export function Positional<T>(information: PositionalInformation<T>): TypedPrope
     return function (target, propertyKey) {
         if (information.name === undefined || information.name === "") throw CLIError.factory.missingRequiredInformation(`${target.constructor.name}.${String(propertyKey)}`, "name");
         if (information.description === undefined || information.description === "") throw CLIError.factory.missingRequiredInformation(`${target.constructor.name}.${String(propertyKey)}`, "description");
-        if (information.position === undefined || information.position) throw CLIError.factory.missingRequiredInformation(`${target.constructor.name}.${String(propertyKey)}`, "position");
+        if (information.position === undefined) throw CLIError.factory.missingRequiredInformation(`${target.constructor.name}.${String(propertyKey)}`, "position");
         if (information.type === undefined) throw CLIError.factory.missingRequiredInformation(`${target.constructor.name}.${String(propertyKey)}`, "type");
 
         const data: OptionMetadata = { ...information, kind: OptionKind.POSITIONAL }
