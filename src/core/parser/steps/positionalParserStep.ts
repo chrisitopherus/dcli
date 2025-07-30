e1fb1f04e315dc22edc8b9c99d93fb0218ff30ec
chrisitopherus <jochchristopher@gmail.com>
2025-07-30 19:16:53 +0200

    implemented PositionalParserStep

--- src/core/parser/steps/positionalParserStep.ts
+++ src/core/parser/steps/positionalParserStep.ts
@@ -1,9 +1,38 @@
 import { ParserStep, ParserStepResult } from "../../../types/core/parser";
+import { OptionKind } from "../../../utility/options/kind";
+import { CLIError } from "../../cliError";
 import { ParserContext } from "../parserContext";
+import { DetermineOptionParserStep } from "./determineOptionParserStep";
 
 export class PositionalParserStep implements ParserStep {
+    public constructor() {
+    }
+
     public handle(context: ParserContext): ParserStepResult {
-        throw new Error("Method not implemented.");
+        const token = context.peek();
+        if (!token) return {
+            success: false,
+            error: CLIError.factory.invalidCommand("no token, positional", "parser")
+        }
+
+        const option = context.consumePositionalOption();
+        if (!option) return {
+            success: false,
+            error: CLIError.factory.invalidCommand("no option at index, positional", "parser")
+        }
+
+        context.addParsedOption({
+            position: option.position,
+            value: option.type(token),
+            kind: OptionKind.POSITIONAL,
+            raw: token,
+            propertyKey: option.propertyKey
+        });
+
+        return {
+            success: true,
+            next: new DetermineOptionParserStep()
+        }
     }
 
 }
