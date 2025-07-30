54901fa78135fab51ddec1a4eb3d2ce8ae6d7e55
chrisitopherus <jochchristopher@gmail.com>
2025-07-30 19:16:13 +0200

    updated and fixed logic for determine which parser step is needed next

--- src/core/parser/steps/determineOptionParserStep.ts
+++ src/core/parser/steps/determineOptionParserStep.ts
@@ -1,28 +1,43 @@
 import { ParserStep, ParserStepResult } from "../../../types/core/parser";
-import { OptionKind } from "../../../utility/options/kind";
 import { ParserContext } from "../parserContext";
-import { FlagParserStep } from "./flagParserStep";
-import { OptionParserStep } from "./optionParserStep";
-import { PositionalParserStep } from "./positionalParserStep";
+import { OptionParserStepFactory } from '../optionParserStepFactory';
+import { LoadedOption } from "../../../types/core/loader";
+import { Maybe } from "../../../types/utility";
 
 export class DetermineOptionParserStep implements ParserStep {
+    private readonly optionStepFactory = new OptionParserStepFactory();
     public constructor() { }
 
     public handle(context: ParserContext): ParserStepResult {
         const command = context.getParsedCommand();
         if (!command) throw new Error("No Command, No parsing.");
-        const token = context.consume();
 
-        const foundOption = token ? context.findOption(token) : undefined;
-
-        if (!foundOption) return {
-            success: true,
-            next: new PositionalParserStep()
-        }
+        const token = context.peek();
+        const foundOption = token ? context.findNamedOption(token) : undefined;
 
         return {
             success: true,
-            next: foundOption.kind === OptionKind.OPTION ? new OptionParserStep(foundOption) : new FlagParserStep()
+            next: this.getOptionStep(foundOption, context)
         };
+    }
+
+    private getOptionStep(option: Maybe<LoadedOption>, context: ParserContext) {
+        if (!option) {
+            option = context.findPositionalOption(context.getPositionalIndex());
+        }
+
+        if (!option) return;
+
+        // TODO: prefer variadic when optional positionals are encountered
+        /*
+            For each positional option:
+            Check if the current option is optional.
+            Look ahead to see if there are:
+            Enough arguments left to still satisfy all required positionals after it, or
+            Any arguments reserved for a variadic positional later.
+            If not enough args remain, skip this optional (set it undefined) and let variadic absorb the rest.
+        */
+
+        return this.optionStepFactory.create(option.kind);
     }
 }
